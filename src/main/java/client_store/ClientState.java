8c2e31d358c52fc664d8f9cff582cd92b9657206
giorgiopea <annatar93@gmail.com>
2017-08-23 14:57:05 +0200

    changing client state

--- src/main/java/client_store/ClientState.java
+++ src/main/java/client_store/ClientState.java
@@ -10,7 +10,7 @@ import java.util.List;
 /**
  * Created by giorgiopea on 24/03/17.
  */
-public class ClientState extends State implements Serializable {
+public class ClientState implements Serializable {
 
     private int tcpPort;
     private List<GamePublicData> availableGames;
